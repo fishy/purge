689c06400214d65485979c133a577cb84fbd1c3d
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-01-09 05:09:06 +0800

    * finished call log purge

--- src/com/yhsif/purge/PurgeActivity.java
+++ src/com/yhsif/purge/PurgeActivity.java
@@ -2,18 +2,23 @@ package com.yhsif.purge;
 
 import android.app.Activity;
 import android.app.AlertDialog.Builder;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.CallLog.Calls;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class PurgeActivity extends Activity
 	implements View.OnClickListener, DialogInterface.OnClickListener, DialogInterface.OnCancelListener {
 
-	public static final long DEFAULT_DAYS = 30;
+	public static final int DEFAULT_DAYS = 30;
 
 	public static final String KEY_DAYS = "days";
 	public static final String KEY_CALL_LOG = "call_log";
@@ -53,9 +58,9 @@ public class PurgeActivity extends Activity
 		editor.commit();
 	}
 
-	protected long getDays() {
+	protected int getDays() {
 		try {
-			return Long.parseLong(((EditText)findViewById(R.id.days)).getText().toString());
+			return Integer.parseInt(((EditText)findViewById(R.id.days)).getText().toString());
 		} catch (NumberFormatException e) {
 			return 0;
 		}
@@ -63,6 +68,49 @@ public class PurgeActivity extends Activity
 
 	protected boolean isChecked(int id) {
 		return ((CheckBox)findViewById(id)).isChecked();
+	}
+
+	protected void purge() {
+		boolean call_log = isChecked(R.id.call_log);
+		boolean sms = isChecked(R.id.sms);
+		boolean locked_sms = isChecked(R.id.locked_sms);
+		int days = getDays();
+		int callsDeleted = 0, smsDeleted = 0;
+
+		Time now = new Time();
+		now.setToNow();
+		now.hour = 0;
+		now.minute = 0;
+		now.second = 0;
+		now.monthDay -= days;
+		long time = now.toMillis(true);
+
+		if (call_log) {
+			String where = new StringBuilder(Calls.DATE)
+				.append(" < ")
+				.append(time)
+				.toString();
+			callsDeleted = getContentResolver().delete(Calls.CONTENT_URI, where, null);
+		}
+
+		String message;
+
+		if (call_log)
+			if (sms)
+				// both
+				message = String.format(getString(R.string.both_deleted), callsDeleted, smsDeleted);
+			else
+				// only calls
+				message = String.format(getString(R.string.call_deleted), callsDeleted);
+		else
+			if (sms)
+				// only sms
+				message = String.format(getString(R.string.sms_deleted), smsDeleted);
+			else
+				// neither
+				message = getString(R.string.nothing);
+
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
 	}
 
 	// for View.OnClickListener
@@ -89,9 +137,7 @@ public class PurgeActivity extends Activity
 				}
 				return;
 			case R.id.do_it:
-				{
-					// TODO
-				}
+				purge();
 				return;
 		}
 	}
