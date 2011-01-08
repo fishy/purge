b1ab27eb8e7ee9b79b9aea5a9a9e61dcc44deb81
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-01-09 00:07:59 +0800

    * initial commit

---
+++ src/com/yhsif/purge/PurgeActivity.java
@@ -0,0 +1,13 @@
+package com.yhsif.purge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PurgeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
